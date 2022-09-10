fb45058f8ff384b787c45e5051dadfe81cd7f29b
Amartya Yadav <iamartyayadav@gmail.com>
2022-09-10 17:54:54 +0530

    home screen cleared

--- lib/screens/home/components/chats.dart
+++ lib/screens/home/components/chats.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quick_chat/screens/chat/chat_room.dart';
 
 import '../../../app_theme.dart';
-import '../../../models/message_model.dart';
 
 class Chats extends StatelessWidget {
   const Chats({
@@ -39,80 +37,80 @@ class Chats extends StatelessWidget {
               ],
             ),
           ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const BouncingScrollPhysics(),
-            itemCount: recentChats.length,
-            itemBuilder: (context, index) {
-              final recentChat = recentChats[index];
-              return Container(
-                margin: const EdgeInsets.only(top: 10,bottom: 10),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 28,
-                      backgroundImage: AssetImage(recentChat.avatar),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        // Navigator.of(context).pushNamed(ChatRoom.routeName,arguments: recentChat.sender);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return ChatRoom(user: recentChat.sender);
-                            },
-                          ),
-                        );
-                      },
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            recentChat.sender.name,
-                            style: MyTheme.heading2.copyWith(
-                              fontSize: 16,
-                            ),
-                          ),
-                          Text(
-                            recentChat.text,
-                            style: MyTheme.bodyText1,
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Spacer(),
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 8,
-                          backgroundColor: MyTheme.kUnreadChatBG,
-                          child: Text(
-                            recentChat.unreadCount.toString(),
-                            style: const TextStyle(
-                              fontSize: 11,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Text(
-                          recentChat.time,
-                          style: MyTheme.bodyTextTime,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
+          // ListView.builder(
+          //   shrinkWrap: true,
+          //   physics: const BouncingScrollPhysics(),
+          //   itemCount: recentChats.length,
+          //   itemBuilder: (context, index) {
+          //     final recentChat = recentChats[index];
+          //     return Container(
+          //       margin: const EdgeInsets.only(top: 10,bottom: 10),
+          //       child: Row(
+          //         children: [
+          //           CircleAvatar(
+          //             radius: 28,
+          //             backgroundImage: AssetImage(recentChat.avatar),
+          //           ),
+          //           const SizedBox(
+          //             width: 20,
+          //           ),
+          //           GestureDetector(
+          //             onTap: () {
+          //               // Navigator.of(context).pushNamed(ChatRoom.routeName,arguments: recentChat.sender);
+          //               // Navigator.push(
+          //               //   context,
+          //               //   MaterialPageRoute(
+          //               //     builder: (context) {
+          //               //       return ChatRoom(user: recentChat.sender);
+          //               //     },
+          //               //   ),
+          //               // );
+          //             },
+          //             child: Column(
+          //               crossAxisAlignment: CrossAxisAlignment.start,
+          //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //               children: [
+          //                 Text(
+          //                   recentChat.sender.name,
+          //                   style: MyTheme.heading2.copyWith(
+          //                     fontSize: 16,
+          //                   ),
+          //                 ),
+          //                 Text(
+          //                   recentChat.text,
+          //                   style: MyTheme.bodyText1,
+          //                 ),
+          //               ],
+          //             ),
+          //           ),
+          //           const Spacer(),
+          //           Column(
+          //             children: [
+          //               CircleAvatar(
+          //                 radius: 8,
+          //                 backgroundColor: MyTheme.kUnreadChatBG,
+          //                 child: Text(
+          //                   recentChat.unreadCount.toString(),
+          //                   style: const TextStyle(
+          //                     fontSize: 11,
+          //                     fontWeight: FontWeight.bold,
+          //                   ),
+          //                 ),
+          //               ),
+          //               const SizedBox(
+          //                 height: 10,
+          //               ),
+          //               Text(
+          //                 recentChat.time,
+          //                 style: MyTheme.bodyTextTime,
+          //               ),
+          //             ],
+          //           ),
+          //         ],
+          //       ),
+          //     );
+          //   },
+          // ),
         ],
       ),
     );
